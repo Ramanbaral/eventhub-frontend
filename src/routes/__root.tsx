1fcf9bf5c9a04739eedc1a2fb48ee05f170073a2
raman baral <ramanisstudent@gmail.com>
2026-08-20 17:44:31 +0545

    feat: create a login page

---
+++ src/routes/__root.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+
+export const Route = createRootRoute({
+  component: RootComponent,
+})
+
+function RootComponent() {
+  return (
+    <React.Fragment>
+      {/* insert header and footer  */}
+      <Outlet />
+      <TanStackRouterDevtools position="bottom-right" />
+    </React.Fragment>
+  )
+}
